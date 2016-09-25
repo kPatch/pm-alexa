90d90d653de5efacfa6cdfe1365811392350fb35
kPatch <irvsteve@gmail.com>
2016-09-25 10:18:14 -0400

    Add Intent logic

--- main/index.js
+++ main/index.js
@@ -56,26 +56,25 @@ exports.handler = function( event, context ) {
                 idList 	= event.request.intent.slots.idlist.value;
                 desc 	= event.request.intent.slots.desc.value;
 
-
                 if(idList.toUpperCase() === 'TO DO'){
                 	idList = '57e6c7b4c816a7f374bd8c12'; // To Do
-                	say += "- TO DO ";
+                	// say += "- TO DO ";
                 } else if(idList.toUpperCase() === 'DEVELOPMENT') {
                     idList = '57e6c7b8255c5b57363c545f'; 
                 } else if(idList.toUpperCase() === 'TESTING') {
                     idList = '57e6c7bbc20f5c811b4c0fa0'; 
                 } else if(idList.toUpperCase() === 'DONE') {
                     idList = '57e6c7bf19cbc254fcc3fd08'; 
-                    say += "- DONE ";
+                    // say += "- DONE ";
                 } else {
-                	idList = '57e6c7b8255c5b57363c545f'; 
-                	say += "- ELSE "
+                	idList = '57e6c7b4c816a7f374bd8c12'; 
+                	// say += "- ELSE "
                 }
 
                 var newCard = {
-                	name: 'name',
-                	desc: 'desc',
-                	idList: '57e6c7b4c816a7f374bd8c12',
+                	name: name,
+                	desc: desc,
+                	idList: idList,
                 	pos: 'top'
                 };
 
@@ -85,7 +84,7 @@ exports.handler = function( event, context ) {
                 		console.log(err.responseBody);
 	  					pop = err.responseBody;
 
-	                   	say += "Error " + name + " with description" + desc + " with idList " + idList + "was successfully created in Trello!";
+	                   	say += "Sorry, I was unable to create the card in Trello!";
 
 	                    // add the state to a session.attributes array
 	                    if (!sessionAttributes.requestList) {
@@ -101,7 +100,7 @@ exports.handler = function( event, context ) {
 
 	  					pop = data.name;
 
-	                   	say = "Card " + name + "with description" + desc + " was successfully created in Trello!";
+	                   	say = "Card " + name + " was successfully created in Trello! What's next?";
 
 	                    // add the state to a session.attributes array
 	                    if (!sessionAttributes.requestList) {
@@ -112,8 +111,6 @@ exports.handler = function( event, context ) {
 	                    // This line concludes the lambda call.  Move this line to within any asynchronous callbacks that return and use data.
 	                    context.succeed({sessionAttributes: sessionAttributes, response: buildSpeechletResponse(say, shouldEndSession) });
                 	}
-              
-
                 });
             }
 
@@ -124,7 +121,7 @@ exports.handler = function( event, context ) {
 
 
         } else if (IntentName === "AMAZON.HelpIntent" ) {
-            say = "Just say the name of a U.S. State, such as Massachusetts or California."
+            say = "Just say the task that you'd like me execute in Trello."
             context.succeed({sessionAttributes: sessionAttributes, response: buildSpeechletResponse(say, shouldEndSession) });
 
         }
